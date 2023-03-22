4d62aebc68398dc4fd6a36e00e19001ab6cf93f5
mbp_xiaolh@163.com <mbp_xiaolh@163.com>
2023-03-22 21:34:03 +0800

    （#1）doc. 解释代码片段 Web应用程序上下文

--- spring-web/src/test/java/org/springframework/web/context/request/WebApplicationContextScopeTests.java
+++ spring-web/src/test/java/org/springframework/web/context/request/WebApplicationContextScopeTests.java
@@ -39,12 +39,18 @@ public class WebApplicationContextScopeTests {
 
 
 	private WebApplicationContext initApplicationContext(String scope) {
+		//创建一个MockServletContext对象，这个对象是一个模拟的Servlet上下文，用于模拟Servlet容器的功能。
 		MockServletContext sc = new MockServletContext();
+		//创建一个GenericWebApplicationContext对象，并将MockServletContext对象作为构造函数的参数，这个对象是一个通用的Web应用程序上下文，用于管理Bean的生命周期。
 		GenericWebApplicationContext ac = new GenericWebApplicationContext(sc);
+		//创建一个GenericBeanDefinition对象，这个对象是一个通用的Bean定义对象，用于描述Bean的属性和行为。
 		GenericBeanDefinition bd = new GenericBeanDefinition();
+		//创建一个GenericBeanDefinition对象，并设置其Bean类为DerivedTestBean，这个对象是一个通用的Bean定义对象，用于描述Bean的属性和行为。
 		bd.setBeanClass(DerivedTestBean.class);
 		bd.setScope(scope);
+		//将Bean定义对象注册到Web应用程序上下文中，使用NAME作为Bean的名称
 		ac.registerBeanDefinition(NAME, bd);
+		//调用Web应用程序上下文的refresh()方法，以便启动应用程序上下文并完成Bean的初始化。
 		ac.refresh();
 		return ac;
 	}
